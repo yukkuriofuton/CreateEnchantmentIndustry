d7c7a34b9cbe96dd04b80054b75ff85250a0a2f7
MarbleGate <marblegatekeeper@outlook.com>
2025-08-31 09:40:04 +0800

    [fix] Fix Spout mending repair does not consume EXP

--- src/main/java/plus/dragons/createenchantmentindustry/mixin/FillingBySpoutMixin.java
+++ src/main/java/plus/dragons/createenchantmentindustry/mixin/FillingBySpoutMixin.java
@@ -60,6 +60,7 @@ public class FillingBySpoutMixin {
             var result = stack.copy();
             stack.shrink(1);
             ExperienceHelper.repairItem(availableXp, serverLevel, result, false);
+            availableFluid.shrink(availableXp);
             cir.setReturnValue(result);
         }
     }
